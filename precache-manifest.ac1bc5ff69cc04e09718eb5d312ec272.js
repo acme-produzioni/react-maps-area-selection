7e301b8a2686fe67fdae86cd35a59b0c67724559
Riccardo <bisark@gmail.com>
2021-04-08 12:36:03 +0200

    Updates

---
+++ precache-manifest.ac1bc5ff69cc04e09718eb5d312ec272.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "db81fa9165092991d6e77bb430a874a6",
+    "url": "./index.html"
+  },
+  {
+    "revision": "2093dffda2c265b3e455",
+    "url": "./static/css/main.8386059c.chunk.css"
+  },
+  {
+    "revision": "5d251e3a18bb30273d77",
+    "url": "./static/js/2.dca18ffd.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "./static/js/2.dca18ffd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2093dffda2c265b3e455",
+    "url": "./static/js/main.caa2a215.chunk.js"
+  },
+  {
+    "revision": "68c7429d3e54c960acff",
+    "url": "./static/js/runtime-main.987ddbcf.js"
+  }
+]);
